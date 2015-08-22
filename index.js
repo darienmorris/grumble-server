c24a6d4e49447c056b32ff910f8c603c5629705e
darienmorris <darien.morris@gmail.com>
2015-08-22 12:36:42 -0400

    Testing sockets

--- index.js
+++ index.js
@@ -4,7 +4,20 @@ var Hapi = require('hapi');
 var server = new Hapi.Server();
 server.connection({ 
     host: 'localhost', 
-    port: 8000 
+    port: 8000 ,
+    labels: ['api']
+});
+
+var io = require('socket.io')(server.select('api').listener);
+
+io.on('connection', function(socket) {
+	console.log("connection!");
+	socket.emit('A user connected');
+
+	socket.on('typing', function(message) {
+		io.emit('typing', message);
+	});
+
 });
 
 // Add the route
