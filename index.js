d7019a15a5666a3174d9b2846d0f8aa4da797075
Darien <darien.morris@gmail.com>
2015-08-23 14:15:34 -0400

    playing around with a db

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 var Hapi = require('hapi');
+var pg = require('pg');
+
+// app.get('/db', function (request, response) {
+//   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+//     client.query('SELECT * FROM test_table', function(err, result) {
+//       done();
+//       if (err)
+//        { console.error(err); response.send("Error " + err); }
+//       else
+//        { response.render('pages/db', {results: result.rows} ); }
+//     });
+//   });
+// })
 
 // Create a server with a host and port
 var server = new Hapi.Server();
