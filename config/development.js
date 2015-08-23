89fab008279e9e545f4e7294e61218595d890b2b
Darien <darien.morris@gmail.com>
2015-08-23 15:19:31 -0400

    adding db credentials for dev

--- config/development.js
+++ config/development.js
@@ -2,7 +2,12 @@ var config = {};
 
 config.db = {
 	sql: {
-
+		user: "gqqugxsrqxnmsl",
+		password: "Y31E6_K1YttB89VnwYFy5_Z9ln",
+		server: "ec2-54-197-253-142.compute-1.amazonaws.com",
+		port: 5432,
+		database: "d6dolfm9itabeu",
+		pool: {}
 	},
 	redis: {
 	
