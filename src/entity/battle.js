9814d9bd789bfac316193fd2725ea0cdbe302fe3
Erik Krieg <erik.c.krieg@gmail.com>
2015-10-03 15:58:37 -0400

    Turn can now restrict player's actions. Turns can also be ended.

--- src/entity/battle.js
+++ src/entity/battle.js
@@ -13,10 +13,6 @@ export default class Battle {
 		// instantiate player class for each userID (this param should be an array of IDs)
 		this.createPlayers();
 
-		// A random player's turn is started
-			// A timer exists that will force the player's turn to end if they do not do so voluntarily before the time expires.
-		// this.establishTurnOrder();
-
 		// Create a socket connection with both players (once connection for each)
 			// Should the player class handle this??
 		this.connectPlayers();
@@ -40,32 +36,26 @@ export default class Battle {
 		if(!this.battleStarted){
 			console.log("establish turn order");
 			this.turn = {};
-			this.turn.first = _.random(0, this.players.length - 1);
+			this.turn.first = this.players[_.random(0, this.players.length - 1)];
 			this.turn.now = this.turn.first;
 			this.battleStarted = true;
-			console.log(this.turn);
 		}
+	}
+
+	toggleTurn(playerID) {
+		var i = this.players.indexOf(playerID);
+		this.turn.now = this.players[Math.abs(i - 1)];
+		console.log(Math.abs(i - 1));
+		console.log(this.turn.now);
 	}
 
 	connectPlayers() {
 		console.log("connect players");
 		let self = this;
-		// For now players is an array of id strings. 
-		// TODO: create player class to use instead.
-
-		/*this.players.forEach(function(player){
-			// listen for socket events for this player
-			// should Battle have access to the server Hapi server instance created in index.js?
-				// If yes, what is the best way to do this? Does socket.io and hapi need to be required in this file still?
-				// Does Battle's constructor need to take the server as an arg?
-
-			
-		});*/
 		
-
 		this.io.on("connection", function(socket) {
+			// onNewPlayerId sets this value for other methods to use.
 			socket.playerID;
-			var playerID;
 
 			// There can only be two players connected in a game,
 			// so kill the socket and return the callback if there is more.
@@ -78,29 +68,8 @@ export default class Battle {
 
 			self.onNewPlayerId(socket);
 			self.onSendMessage(socket);
+			self.onEndTurn(socket);
 			self.onDisconnect(socket);
-
-			/*socket.on("player id", function(id){
-				playerID = id;
-				self.players.push(id);
-				if(self.players.length == 2){
-					self.establishTurnOrder();
-				}
-				self.io.emit("player count", self.players.length);
-			});*/
-
-			/*socket.on("send message", function(data) {
-				if(playerID == self.turn.now)
-				let message = data.id + ": " + data.message;
-				self.io.emit("send message", message);
-			});*/
-
-			/*socket.on('disconnect', function(){				
-				let i = self.players.indexOf(playerID);
-				self.players.splice(i, 1);
-				console.log('user disconnected');
-				socket.disconnect();
-			});*/
 		});
 	}
 
@@ -118,14 +87,28 @@ export default class Battle {
 
 	onSendMessage(socket) {
 		let self = this;
-		socket.on("send message", function(data) {
-			//if(socket.playerID == self.turn.now)
-			let message = data.id + ": " + data.message;
-			self.io.emit("send message", message);
+		socket.on("send message", function(message) {
+			if(socket.playerID == self.turn.now){
+				self.emitMessage(socket, message);
+			} else {
+				self.emitNotYourTurn(socket);
+			}		
 		});
 	}
 
-	onDisconnect(socket){
+	onEndTurn(socket) {
+		let self = this;
+		socket.on("end turn", function(){
+			if(socket.playerID == self.turn.now){
+				self.toggleTurn(socket.playerID);
+				self.emitMessage(socket, "has ended their turn");
+			} else {
+				self.emitNotYourTurn(socket);
+			}
+		});
+	}
+
+	onDisconnect(socket) {
 		let self = this;
 		socket.on('disconnect', function(){				
 			let i = self.players.indexOf(socket.playerID);
@@ -133,6 +116,15 @@ export default class Battle {
 			console.log('user disconnected');
 			socket.disconnect();
 		});
+	}
+
+	emitMessage(socket, message) {
+		message = socket.playerID + ": " + message;
+		this.io.emit("send message", message);
+	}
+
+	emitNotYourTurn(socket) {
+		this.io.emit(socket.playerID + " not your turn");
 	}
 
 }
